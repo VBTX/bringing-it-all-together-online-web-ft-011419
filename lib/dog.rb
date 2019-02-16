4c9a863fdc3555582ff5517ba1d8251919e08708
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 01:42:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,5 +25,17 @@ class Dog
   end
 
   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_row.id() FROM dogs")[0][0]
   end
+  end
+
 end
