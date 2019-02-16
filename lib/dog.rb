51fd053e427b63f0e6570a58f174a902a308697b
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 02:14:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,11 +58,12 @@ class Dog
     DB[:conn].execute(sql, name).map do |row|
     self.new_from_db(row)
   end.first
+  end
 
   def update
     sql = "UPDATE dogs SET name =?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-end
+
 end
