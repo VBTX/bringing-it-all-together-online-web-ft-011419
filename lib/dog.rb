ae5bbde80f666e6a6a89efcf4b77d6e841a2d34a
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 03:13:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,7 @@ class Dog
     dog = DB[:conn].execute("SELECT*FROM dogs WHERE name = ? AND breed = ?", name, breed)
 
     if !dog.empty?
-      new_dog = self.new_from_db(dog)
+      new_dog = Dog.new(name, breed)
     else
       new_dog = self.create({:name => name, :breed => breed})
     end
