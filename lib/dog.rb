02a12aecb69f2df5dbf5504b4b943d64fa26cd25
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 01:35:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,9 @@
 class Dog
   attr_accessor :name, :breed
-  def initialize
+  attr_reader :id
+  def initialize (id = nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
   end
 end
