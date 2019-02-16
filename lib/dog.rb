96e5ba75441716f624ee4a497998d466293eb6fc
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 01:53:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = Dog.new
+    new_dog = Dog.new(id:row[0], name:row[1], breed:row[2])
     new_dog.id = row[0]
     new_dog.name = row[1]
     new_dog.breed = row[2]
