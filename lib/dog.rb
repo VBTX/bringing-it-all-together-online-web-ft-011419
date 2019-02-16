edad1b868d7bb8aeae61bf3c9cf34a2c08ef9bea
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 03:29:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,6 @@ class Dog
 
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    binding.pry
     if !dog.empty?
       dog_data = dog[0]
       new_dog = self.new_from_db(dog_data)
