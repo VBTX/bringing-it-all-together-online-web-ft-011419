20a12549079a784bfbdf7fbf6ec2e0278a196dcb
Victoria B <viktorija.bourassa@gmail.com>
2019-02-16 03:15:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
     if !dog.empty?
       new_dog = Dog.new(name, breed)
     else
-      new_dog = self.create(name:name, breed:breed)
+      new_dog = self.create({:name => name, :breed => breed})
     end
     new_dog
   end
